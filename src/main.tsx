c8b21bdb29c4ed28ec04ee6a7ca0fc07a0288519
vr150712-beep <vr150712@gmail.com>
2026-05-19 18:30:33 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fontsource-variable/bricolage-grotesque'
-import '@fontsource/dm-mono'
-import '@fontsource/dm-mono/500.css'
 import './index.css'
 import App from './App'
 
